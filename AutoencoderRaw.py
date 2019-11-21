107dbd48bf2a69b0df0ff52ca291e55e7cdc3e6e
vidarsolli <vidar.solli@me.com>
2019-11-21 16:50:04 +0100

    Autoencoder - Running out of memory

--- AutoencoderRaw.py
+++ AutoencoderRaw.py
@@ -50,27 +50,36 @@ for o, a in myOpts:
 with open(json_file) as file:
     cp = json.load(file)
 
-# Build training set
+try:
+    x_train = np.load("x_train.npy")
+except:
+    # Build training set
+    audio_files = get_audiofiles(cp["audio_folder"])
+    x_train = np.array([])
+    for file in audio_files:
+        audio_samples, sample_rate = librosa.load(file)
+        audio_samples=librosa.resample(audio_samples, sample_rate, cp["sample_rate"])
+        window_size = int(cp["short_term"] * cp["sample_rate"])
+        step_size = int(cp["step_size"] * cp["sample_rate"])
+        print("Window_size: ", window_size, "Step_size: ", step_size)
+        no_of_samples = int((audio_samples.shape[0]-window_size)/step_size)-1
+        # dt = time between each feature vector
+        dt = step_size/sample_rate
+        print("Extracting features from ", file, "# samples: ", audio_samples.shape, " sr: ", cp["sample_rate"], " dt: ", dt, "# features: ", no_of_samples)
 
-audio_files = get_audiofiles(cp["audio_folder"])
-x_train = np.array([])
-for file in audio_files:
-    audio_samples, sample_rate = librosa.load(file)
-    audio_samples=librosa.resample(audio_samples, sample_rate, cp["sample_rate"])
-    window_size = int(cp["short_term"] * cp["sample_rate"])
-    step_size = int(cp["step_size"] * cp["sample_rate"])
-    print("Window_size: ", window_size, "Step_size: ", step_size)
-    no_of_samples = int((audio_samples.shape[0]-window_size)/step_size)-1
-    # dt = time between each feature vector
-    dt = step_size/sample_rate
-    print("Extracting features from ", file, "# samples: ", audio_samples.shape, " sr: ", cp["sample_rate"], " dt: ", dt, "# features: ", no_of_samples)
+        for i in range(no_of_samples):
+            y = audio_samples[(i*step_size):(i*step_size+window_size)]
+            x_train = np.append(x_train, y)
 
-    for i in range(no_of_samples):
-        y = audio_samples[(i*step_size):(i*step_size+window_size)]
-        x_train = np.append(x_train, y)
+    x_train = np.reshape(x_train, (int(len(x_train)/window_size), window_size, 1))
+    x_train += 1
+    x_train *= 0.4
+    min = np.min(x_train)
+    max = np.max(x_train)
+    print(min, max)
+    print(x_train.shape)
+    np.save("x_train", x_train)
 
-x_train = np.reshape(x_train, (int(len(x_train)/window_size), window_size, 1))
-print(x_train.shape)
 input = Input(shape=(int(cp["sample_rate"]*cp["short_term"]), 1))  # adapt this if using `channels_first` image data format
 
 x = Conv1D(cp["filter1_size"], cp["kernal1_size"], activation=cp["activation"], strides=cp["strides"], data_format='channels_last', padding='same')(input)
@@ -88,7 +97,7 @@ x = Conv1D(cp["filter2_size"], cp["kernal2_size"], activation=cp["activation"], 
 x = UpSampling1D(2)(x)
 x = Conv1D(cp["filter1_size"], cp["kernal1_size"], activation=cp["activation"], strides=cp["strides"], padding='same')(x)
 x = UpSampling1D(2)(x)
-decoded = Conv1D(1, 3, activation='tanh', padding='same')(x)
+decoded = Conv1D(1, 3, activation='sigmoid', padding='same')(x)
 
 print(type(decoded))
 
@@ -100,11 +109,11 @@ autoencoder.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accu
 
 # Start the TensorBoard server by: tensorboard --logdir=/tmp/autoencoder
 # and navigate to: http://0.0.0.0:6006
+# In case, add: callbacks=[TensorBoard(log_dir='/tmp/autoencoder')]
 
 if cp["train"]:
     autoencoder.fit(x_train, x_train,
                 epochs=cp["epochs"],
                 batch_size=cp["batch_size"],
-                shuffle=True,
-                validation_split=cp["validation_split"],
-                callbacks=[TensorBoard(log_dir='/tmp/autoencoder')])
+                verbose=True,
+                validation_split=cp["validation_split"])
