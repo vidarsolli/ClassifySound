660dae5f1f953aa6dbb942a815647eee0a403948
vidarsolli <vidar.solli@me.com>
2019-11-21 11:25:15 +0100

    Autoencoder - reading audio files

--- AutoencoderRaw.py
+++ AutoencoderRaw.py
@@ -1,31 +1,103 @@
-from keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D
+from keras.layers import Input, Dense, Conv1D, MaxPooling1D, UpSampling1D
 from keras.models import Model
 from keras import backend as K
 from keras.utils import plot_model
+from keras.callbacks import TensorBoard
+import json
+import time
+import sys, getopt
+import librosa
+import os
+
+
 import numpy as np
 
-input_img = Input(shape=(28, 28, 1))  # adapt this if using `channels_first` image data format
+"""
+AutodecoderRaw will train a 1D convolutional autoencoder with raw audio signals
+and cluster the encoder vector to see if clusters contains sound with similar characteristics.
+The parameters for the network are stored in a .json file
 
-x = Conv2D(16, (3, 3), activation='relu', padding='same')(input_img)
-x = MaxPooling2D((2, 2), padding='same')(x)
-x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
-x = MaxPooling2D((2, 2), padding='same')(x)
-x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
-encoded = MaxPooling2D((2, 2), padding='same')(x)
+The audiofiles to be used are stored in a folder (folder to be stated in the .json file).
+
+Usage:  python3 AutoencoderRaw -i <filenam>.json
+
+"""
+
+json_file = "./config.json"
+# Return a list of audio files
+def get_audiofiles(folder):
+    list_of_audio = []
+    for file in os.listdir(folder):
+        if file.endswith(".wav"):
+            directory = "%s/%s" % (folder, file)
+            list_of_audio.append(directory)
+    return list_of_audio
+
+
+# Get the input json file
+try:
+    myOpts, args = getopt.getopt(sys.argv[1:], "i:")
+except getopt.GetoptError as e:
+    print(str(e))
+    print("Usage: %s -i <json_file>" % sys.argv[0])
+    sys.exit(2)
+
+for o, a in myOpts:
+    if o == '-i':
+        json_file = a
+
+with open(json_file) as file:
+    cp = json.load(file)
+
+# Build training set
+
+audio_files = get_audiofiles(cp["audio_folder"])
+for file in audio_files:
+    audio_samples, sample_rate = librosa.load(file)
+    audio_samples=librosa.resample(audio_samples, sample_rate, cp["sample_rate"])
+    window_size = int(cp["short_term"] * cp["sample_rate"])
+    step_size = int(cp["step_size"] * cp["sample_rate"])
+    print("Window_size: ", window_size, "Step_size: ", step_size)
+    no_of_samples = int((audio_samples.shape[0]-window_size)/step_size)-1
+    # dt = time between each feature vector
+    dt = step_size/sample_rate
+    print("Extracting features from ", file, "# samples: ", audio_samples.shape, " sr: ", cp["sample_rate"], " dt: ", dt, "# features: ", no_of_samples)
+    for i in range(no_of_samples):
+
+
+input = Input(shape=(int(cp["sample_rate"]*cp["short_term"]), 1))  # adapt this if using `channels_first` image data format
+
+x = Conv1D(cp["filter1_size"], cp["kernal1_size"], activation=cp["activation"], padding='same')(input)
+x = MaxPooling1D(2, padding='same')(x)
+x = Conv1D(cp["filter2_size"], cp["kernal2_size"], activation=cp["activation"], padding='same')(x)
+x = MaxPooling1D(2, padding='same')(x)
+x = Conv1D(cp["filter3_size"], cp["kernal3_size"], activation=cp["activation"], padding='same')(x)
+encoded = MaxPooling1D(2, padding='same')(x)
 
 # at this point the representation is (4, 4, 8) i.e. 128-dimensional
 
-x = Conv2D(8, (3, 3), activation='relu', padding='same')(encoded)
-x = UpSampling2D((2, 2))(x)
-x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
-x = UpSampling2D((2, 2))(x)
-x = Conv2D(16, (3, 3), activation='relu')(x)
-x = UpSampling2D((2, 2))(x)
-decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
+x = Conv1D(cp["filter3_size"], cp["kernal3_size"], activation=cp["activation"], padding='same')(encoded)
+x = UpSampling1D(2)(x)
+x = Conv1D(cp["filter2_size"], cp["kernal2_size"], activation=cp["activation"], padding='same')(x)
+x = UpSampling1D(2)(x)
+x = Conv1D(cp["filter1_size"], cp["kernal1_size"], activation=cp["activation"])(x)
+x = UpSampling1D(2)(x)
+decoded = Conv1D(1, 3, activation='sigmoid', padding='same')(x)
 
 
-autoencoder = Model(input_img, decoded)
+autoencoder = Model(input, decoded)
 
 plot_model(autoencoder, show_shapes=True, expand_nested=True, to_file='model.png')
 
 autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
+
+# Start the TensorBoard server by: tensorboard --logdir=/tmp/autoencoder
+# and navigate to: http://0.0.0.0:6006
+"""
+autoencoder.fit(x_train, x_train,
+                cp["epochs"]=50,
+                cp["batch_size"]=128,
+                shuffle=True,
+                validation_data=(x_test, x_test),
+                callbacks=[TensorBoard(log_dir='/tmp/autoencoder')])
+"""
